271f3b011b19ec1c1311b80821be004c2802b171
Aprian Bwire <aprythomas5@gmail.com>
2023-04-01 19:12:30 +0300

    added data types

--- 0x01-data_types/implementing.dart
+++ 0x01-data_types/implementing.dart
@@ -3,6 +3,10 @@ void main()
     int value = 54; //it is a signed int takes both positive and negative values
     String name = 'Aprian';//Sequence of characters
     bool isTrue = false;
-    
+    List fruits = ['apple','banana','mango'];
+    print(value);
+    print(name);
+    print(isTrue);
+    print(fruits);
 
 }
