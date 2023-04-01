0a46594831ab7961f32b06d2564b9074b32f3d86
Aprian Bwire <aprythomas5@gmail.com>
2023-04-01 19:07:19 +0300

    First commit

---
+++ 0x01-data_types/implementing.dart
@@ -0,0 +1,7 @@
+void main()
+{
+    int value = 54; //it is a signed integer taking both +ve and -ve values
+    unsigned int = 23; //takes only positive values
+    String name = 'Aprian';//Sequence of characters
+    
+}
