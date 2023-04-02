069b9580480fd8498e67f959cff0f1f19a8f7f89
Aprian Bwire <aprythomas5@gmail.com>
2023-04-02 22:51:14 +0300

    Added method in tr class

--- 0x04-object_oriented_prog2/teacher_student.dart
+++ 0x04-object_oriented_prog2/teacher_student.dart
@@ -4,7 +4,18 @@ class teacher
   int? age;
   String? subject;
 
-  
+  teacher(String name, int age, String subject)
+  {
+    this.name = name;
+    this.age = age;
+    this.subject = subject;
+  }
+
+  void print_tr_info()
+  {
+    print("Name: $name\nage: $age\nSubject: $subject");
+
+  }
 }
 class student
 {
