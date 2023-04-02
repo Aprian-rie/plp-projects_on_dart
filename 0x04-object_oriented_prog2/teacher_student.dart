2c238537bc9d9cdffe5abea29451631bee32f6e6
Aprian Bwire <aprythomas5@gmail.com>
2023-04-02 22:54:11 +0300

    Added method in student class

--- 0x04-object_oriented_prog2/teacher_student.dart
+++ 0x04-object_oriented_prog2/teacher_student.dart
@@ -22,4 +22,15 @@ class student
   String? name;
   int? age;
   int? gradelevel;
+
+  student(String name, int age, int gradelevel)
+  {
+    this.name = name;
+    this.age = age;
+    this.gradelevel = gradelevel;
+  }
+  void print_student_info()
+  {
+    print("name: $name\nAge: $age\nGrade-level: $gradelevel");
+  }
 }
